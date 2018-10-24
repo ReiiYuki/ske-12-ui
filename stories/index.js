18be5efd3a3ada3dc982e4a430280cb73e2236d6
ReiiYuki <iguru.shou.5410070@gmail.com>
2018-10-24 22:15:27 +0700

    Try precommit

--- stories/index.js
+++ stories/index.js
@@ -5,7 +5,7 @@ import { storiesOf } from '@storybook/react';
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+    <Button onClick={action('clicked')}>Hello Button</Button> 
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
